7655998076d4118fdfd3cd4b55422dc911a9c13a
Moritz Schmale <narrow.m@gmail.com>
2015-10-05 20:27:57 +0200

    Fixed config for temperature units

--- config.example.js
+++ config.example.js
@@ -6,8 +6,8 @@ var config = {
     cloudinaryApiSecret: "",
     cloudinaryName: "",
     slackSlackPort: 3000,
-    metric: ['cm', 'meters', 'centimeters', 'liters', 'liter', 'L', 'dm', 'm', 'km/h', 'kmh', 'C', 'degrees C', 'º C', 'ºC'],
-    imperial: ['"', 'in', 'inches', 'inch', 'ft', 'feet', 'mile', 'miles', 'mph', 'F', 'degrees F', 'º F', 'ºF']
+    metric: ['cm', 'meters', 'centimeters', 'liters', 'liter', 'L', 'dm', 'm', 'km/h', 'kmh', 'C', 'degrees C', '° C', '°C'],
+    imperial: ['"', 'in', 'inches', 'inch', 'ft', 'feet', 'mile', 'miles', 'mph', 'F', 'degrees F', '° F', '°F']
 };
 
 module.exports = config;
