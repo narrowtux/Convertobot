8a14d9a32be1d95a1c0582df2ae1e682e58a7fb3
Moritz Schmale <narrow.m@gmail.com>
2015-11-20 16:56:29 +0100

    Possible fix for slack reconnection troubles

--- index.js
+++ index.js
@@ -125,6 +125,8 @@ function wolframQuery(query, full, message) {
     })
 }
 
+var messages = {};
+
 slack.on("open", function() {
     logger.info("ready");
 
@@ -143,33 +145,33 @@ slack.on("open", function() {
                 });
             }
         }
-    })
+    });
+    slack.on("message", function(message) {
+        if (message.text) {
+            var msg = new Message({
+                channel: message.channel
+            }, slack);
+            messages[message.channel + message.ts] = {
+                message: msg,
+                time: Date.now()
+            };
+            onMessage(message, msg);
+        } else if (message.subtype && message.subtype == "message_changed") {
+            msg = messages[message.channel + message.message.ts];
+            if (msg) {
+                onMessage(message.message, msg.message);
+            }
+        } else if (message.subtype && message.subtype == "message_deleted") {
+            msg = messages[message.channel + message.deleted_ts];
+            if (msg && msg.message) {
+                msg.message.remove();
+            }
+        }
+    });
 });
 
-var messages = {};
 
-slack.on("message", function(message) {
-    if (message.text) {
-        var msg = new Message({
-            channel: message.channel
-        }, slack);
-        messages[message.channel + message.ts] = {
-            message: msg,
-            time: Date.now()
-        };
-        onMessage(message, msg);
-    } else if (message.subtype && message.subtype == "message_changed") {
-        msg = messages[message.channel + message.message.ts];
-        if (msg) {
-            onMessage(message.message, msg.message);
-        }
-    } else if (message.subtype && message.subtype == "message_deleted") {
-        msg = messages[message.channel + message.deleted_ts];
-        if (msg && msg.message) {
-            msg.message.remove();
-        }
-    }
-});
+
 
 setInterval(function () {
     var toRemove = [];
